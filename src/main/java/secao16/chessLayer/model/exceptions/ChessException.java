b69664a14b14e152dd72d0b6cf5d7405f80612aa
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-07-22 16:48:01 -0300

    feat: create ChessException

---
+++ src/main/java/secao16/chessLayer/model/exceptions/ChessException.java
@@ -0,0 +1,9 @@
+package secao16.chessLayer.model.exceptions;
+
+public class ChessException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public ChessException(String msg) {
+        super(msg);
+    }
+}
