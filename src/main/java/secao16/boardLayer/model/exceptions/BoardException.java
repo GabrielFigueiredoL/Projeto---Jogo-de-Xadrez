cf9d38ba8c7f8c6307abb96525ce368db815acfb
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-07-22 16:48:11 -0300

    feat: create BoardException

---
+++ src/main/java/secao16/boardLayer/model/exceptions/BoardException.java
@@ -0,0 +1,9 @@
+package secao16.boardLayer.model.exceptions;
+
+public class BoardException extends Exception {
+    private static final long serialVersionUID = 2L;
+
+    public BoardException(String msg) {
+        super(msg);
+    }
+}
