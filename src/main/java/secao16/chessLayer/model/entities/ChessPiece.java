3906a8b618759ea47e6d9699cc2af642d2654db4
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-07-25 00:35:24 -0300

    feat: implement ChessPiece Class

---
+++ src/main/java/secao16/chessLayer/model/entities/ChessPiece.java
@@ -0,0 +1,24 @@
+package secao16.chessLayer.model.entities;
+
+import secao16.boardLayer.model.entities.Board;
+import secao16.boardLayer.model.entities.Piece;
+import secao16.boardLayer.model.entities.Position;
+import secao16.chessLayer.model.enums.Color;
+
+public abstract class ChessPiece extends Piece {
+    private Color color;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    protected boolean isThereOpponentPiece(Position position) {
+        ChessPiece piece = (ChessPiece) getBoard().piece(position);
+        return piece != null && piece.getColor() != color;
+    }
+}
