1a265d911d91ff25d96ad99055a7066549f1d5db
gabrielfigueiredol <gabrieellfigueiredo@gmail.com>
2024-07-25 01:03:44 -0300

    feat: implement Bishop ChessPiece and update board to default chess settings

--- src/main/java/secao16/chessLayer/model/entities/ChessMatch.java
+++ src/main/java/secao16/chessLayer/model/entities/ChessMatch.java
@@ -3,6 +3,7 @@ package secao16.chessLayer.model.entities;
 import secao16.boardLayer.model.entities.Board;
 import secao16.boardLayer.model.entities.Piece;
 import secao16.boardLayer.model.entities.Position;
+import secao16.chessLayer.model.entities.chessPieces.Bishop;
 import secao16.chessLayer.model.entities.chessPieces.King;
 import secao16.chessLayer.model.entities.chessPieces.Rook;
 import secao16.chessLayer.model.enums.Color;
@@ -69,19 +70,17 @@ public class ChessMatch {
     }
 
     private void initialSetup() {
-        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 1, new King(board, Color.WHITE));
+        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
 
-        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 8, new King(board, Color.BLACK));
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
 
     }
 }
